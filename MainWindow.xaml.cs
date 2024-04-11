d5467d1bf54667da1f79ad21b71a46807aad4bcb
Levi-24 <leventemajor36@gmail.com>
2024-04-11 12:12:48 +0200

    Finish

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,26 +20,31 @@ namespace Calculator
         private void Finish_Click(object sender, RoutedEventArgs e)
         {
             string equation = lblEquation.Content.ToString();
-            if (equation.Contains("^"))
+
+            try
             {
-                string[] temp = equation.Split("^");
-                double pow = Math.Pow(Convert.ToDouble(temp[0]), Convert.ToDouble(temp[1]));
-                lblOutput.Content = pow.ToString();
-            }
-            else
-            {
-                if (equation.Contains("√"))
+                if (equation.Contains("^"))
                 {
-                    string temp = equation.Replace("√","");
-                    double root = Math.Sqrt(Convert.ToDouble(temp));
-                    lblOutput.Content = root.ToString();
+                    string[] temp = equation.Split("^");
+                    double pow = Math.Pow(Convert.ToDouble(temp[0]), Convert.ToDouble(temp[1]));
+                    lblOutput.Content = pow.ToString();
                 }
                 else
                 {
-                    var result = new DataTable().Compute(equation, null);
-                    lblOutput.Content = result.ToString();
+                    if (equation.Contains("√"))
+                    {
+                        string temp = equation.Replace("√", "");
+                        double root = Math.Sqrt(Convert.ToDouble(temp));
+                        lblOutput.Content = root.ToString();
+                    }
+                    else
+                    {
+                        var result = new DataTable().Compute(equation, null);
+                        lblOutput.Content = result.ToString();
+                    }
                 }
             }
+            catch (Exception){lblOutput.Content = "Math Error!";}
         }
 
         private void Button_Clear(object sender, RoutedEventArgs e)
@@ -71,6 +76,11 @@ namespace Calculator
                 BtnRoot.Visibility = Visibility.Hidden;
             }
 
+        }
+
+        private void Button_About(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Corrently aavailiable functions: \n Summation, Substraction, Multiplication, Division.\n These operations can be used in in long equations or by themselves and the order of operations apply to them. You can also work with fractions.\n\n There is an availiable Exponentiation and Square root function but they can be only used by themselves. To access them tick the Scientific checkbox \n\n The CA button means: Clear All, and the C button means: Clear (it removes the last pressed button).","Calculator",MessageBoxButton.OK,MessageBoxImage.Information);
         }
     }
 }
